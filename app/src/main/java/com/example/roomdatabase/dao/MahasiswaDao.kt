bcf28694c7d2622a21180be31b2e01a02f1154d1
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-16 15:20:29 +0700

    menambahkan fungsi delete dan update mahasiswa

--- app/src/main/java/com/example/roomdatabase/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/dao/MahasiswaDao.kt
@@ -1,9 +1,25 @@
 package com.example.roomdatabase.dao
 
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.roomdatabase.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query ("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim : String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
