a261ef2882471a41a11b28c34eba398e76fb4ac8
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-17 03:22:13 +0700

    membuat data class untuk menampung data yg akan ditampilkan di UI

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.roomdatabase.ui.viewmodel
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+){
+    val isUiEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+    val isUiNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
