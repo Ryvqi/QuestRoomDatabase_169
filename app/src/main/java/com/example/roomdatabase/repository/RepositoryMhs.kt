e6cc2074af43ef9bf4fad1b11a7d52d703eb6f46
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-16 15:31:48 +0700

    menambahkan fungsi untuk mengelola data mahasiswa

--- app/src/main/java/com/example/roomdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomdatabase/repository/RepositoryMhs.kt
@@ -1,7 +1,13 @@
 package com.example.roomdatabase.repository
 
 import com.example.roomdatabase.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
+
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
